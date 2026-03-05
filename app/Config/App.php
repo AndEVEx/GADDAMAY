5ae36ab1c4817d1c1b6ae2df442da1008ef9b9c8
SMKN2 Indramayu <github@smkn2indramayu.sch.id>
2026-03-05 07:48:46 +0700

    fix: change baseURL from localhost to production URL
    CRITICAL: baseURL was http://localhost:8080/ causing all CSS/JS/images
    to fail on deployed site. Changed to https://absen.smkn2indramayu.sch.id/
    Added localhost:8080 to allowedHostnames for local development.

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'https://absen.smkn2indramayu.sch.id/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
@@ -31,7 +31,7 @@ class App extends BaseConfig
      * @var string[]
      * @phpstan-var list<string>
      */
-    public array $allowedHostnames = [];
+    public array $allowedHostnames = ['localhost:8080'];
 
     /**
      * --------------------------------------------------------------------------
